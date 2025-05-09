88f682a69679e747282ea38ad6fb44b215e68565
katarinamarj <katarinamarjanovic02@gmail.com>
2025-05-09 23:02:03 +0200

    Created nav menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,9 +4,30 @@ const year = new Date().getFullYear()
 </script>
 
 <template>
-  <RouterLink to="/">Home</RouterLink> | 
-  <RouterLink to="/about"> About</RouterLink> | 
-  <RouterLink to="/list"> Books</RouterLink> 
-  <RouterView />
-  <p>&copy {{ year }} Library</p>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+      <div class="container-fluid">
+        <RouterLink class="navbar-brand fs-4" to="/" > <i class="fa-solid fa-book-open-reader"></i> Library        
+        </RouterLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/" exact-active-class="active">Home </RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/about" exact-active-class="active">About </RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/list" exact-active-class="active">List </RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <RouterView />
+    <p class="text-center mt-3">&copy {{ year }} Library system</p>
+  </div>
 </template>
