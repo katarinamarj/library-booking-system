e64e0b690ecba1a3911d847e571c70c999427491
katarinamarj <katarinamarjanovic02@gmail.com>
2025-05-13 13:01:23 +0200

    Added defineRequest function to handle responses and errors

---
+++ backend/src/utils.ts
@@ -0,0 +1,19 @@
+import type { Response } from "express";
+
+export async function defineRequest(res: Response, callback: Function){
+    const status = 200
+    try {
+        const data = await callback()
+        if( data == null) {
+            res.status(204).send()
+            return
+        }
+        res.json(data)
+    } catch(e: any){
+        const code = e.message == 'NOT_FOUND' ? 404 : 500
+        res.status(code).json({
+            message: e.message ?? 'ERROR',
+            timestamp: new Date()
+        })
+    }
+}
